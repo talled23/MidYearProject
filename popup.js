2ce51223ba8ab4651b21388081b074f461c7a685
Tal Ledeniov <tal.ledeniov@gmail.com>
2020-02-02 15:48:58 -0500

    push27

--- popup.js
+++ popup.js
@@ -1,1 +1,1 @@
-document.getElementById("rickardo").addEventListener("click", function() { window.open("https://www.latlmes.com/tech/comp-sci-lab-answers-1", "_blank")})
+window.getElementById("rickardo").addEventListener("click", function() { window.open("https://www.latlmes.com/tech/comp-sci-lab-answers-1", "_blank")})
