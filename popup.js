4bec202992dab3cba7b1afaf8e828ff0d26ed9db
Tal Ledeniov <tal.ledeniov@gmail.com>
2020-02-04 11:40:35 -0500

    push39

--- popup.js
+++ popup.js
@@ -4,6 +4,12 @@ function counter() {
     document.getElementById('Counter').style.padding = "16px 45px 16px 45px";
 }
 
+function pythagor() {
+    num1 = int(document.getElementById("input1").value)
+    num2 = int(document.getElementById("input2").value)
+    document.getElementById("answer").innerHTML=num1+num2;
+}
+
 document.getElementById("Counter").addEventListener("click", counter)
 
 function encrypt() {
